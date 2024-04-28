8cf1c7826a3dd89a6f4089c4e1a5bff4f5ba1928
Itode fortune <fortune.nwuneke@gmail.com>
2024-04-28 17:48:22 +0100

    no scroll effect

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,15 @@ import { Link } from "react-router-dom";
 // import HumanVsHuman from "../components/HVH";
 
 const HomePage = () => {
+  useEffect(() => {
+    // Add the 'no-scroll' class to the body element when the component mounts
+    document.body.classList.add("no-scroll");
+
+    // Remove the 'no-scroll' class from the body element when the component unmounts
+    return () => {
+      document.body.classList.remove("no-scroll");
+    };
+  }, []);
   return (
     <div className="home-page">
       <h1>Welcome to the game</h1>
